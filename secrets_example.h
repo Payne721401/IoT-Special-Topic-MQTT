d77ef27c110f08d4ddf52eb6be88bd55f5945083
Payne721401 <134119880+Payne721401@users.noreply.github.com>
2026-03-03 16:56:43 +0800

    Add secrets_example.h for WiFi and MQTT configuration

---
+++ secrets_example.h
@@ -0,0 +1,12 @@
+// secrets_example.h
+#ifndef SECRETS_H
+#define SECRETS_H
+
+#define WIFI_SSID "YOUR_WIFI_SSID"
+#define WIFI_PASSWORD "YOUR_WIFI_PASSWORD"
+
+#define MQTT_BROKER_IP "192.168.X.X"
+#define MQTT_PORT 1883
+#define MQTT_TOPIC "esp32/output"
+
+#endif
